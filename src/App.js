d833e9e8f4383fa96969b5ab58bd9824b705703e
Thien DAO <thien1997nus@gmail.com>
2022-11-21 15:12:57 +0700

    react prop todos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import TodoList from './TodoList';
 function App() {
+  const [todos, setTodos] = useState(['To do 1','To do 2'])
   return (
     <>
-      <TodoList />
-      <input type="text"></input>
+      <TodoList todos={todos} />
+      <input type="text" />
       <button>Add todo</button>
       <button>Clear completed</button>
       <div>0 left to do</div>
