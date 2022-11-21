0adc1f90c75cc06e58d37f9bed88cce15046f079
Thien DAO <thien1997nus@gmail.com>
2022-11-21 19:48:34 +0700

    add uuid {v4 as uuidv4} from 'uuid'

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import logo from './logo.svg';
 import React, { useState, useRef } from 'react';
-import './App.css';
 import TodoList from './TodoList';
+import {v4 as uuidv4} from 'uuid';
 function App() {
-  const [todos, setTodos] = useState([{id:1, name:'To do 1',complete: false},
-{id:1, name:'To do 2', complete: true}])
+  const [todos, setTodos] = useState([{id:uuidv4(), name:'To do 1',complete: false}])
   const todoNameRef = useRef()
   function handleAddTodo(e){
     const name = todoNameRef.current.value
     if (name === '') return
     setTodos(prevTodos =>{
-      return [...prevTodos, {id: 1, name: name, complete:false}]
+      return [...prevTodos, {id: uuidv4(), name: name, complete:false}]
     })
     todoNameRef.current.value = null
   }
