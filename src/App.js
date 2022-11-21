76b6c726256d7223a7c66d4fabf6f96a4f533430
Thien DAO <thien1997nus@gmail.com>
2022-11-21 19:15:42 +0700

    func handleTodo use var NameRef w useRef()

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import logo from './logo.svg';
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
 import TodoList from './TodoList';
 function App() {
   const [todos, setTodos] = useState([{id:1, name:'To do 1',complete: false},
 {id:1, name:'To do 2', complete: true}])
+  const todoNameRef = useRef()
+  function handleAddTodo(e){
+    const name = todoNameRef.current.value
+    if (name === '') return
+    console.log(name)
+  }
+
   return (
     <>
       <TodoList todos={todos} />
-      <input type="text" />
-      <button>Add todo</button>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add todo</button>
       <button>Clear completed</button>
       <div>0 left to do</div>
     </>
