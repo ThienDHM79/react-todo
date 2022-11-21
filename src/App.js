adca2b3ed559e1decf55919545b43dda8a184001
Thien DAO <thien1997nus@gmail.com>
2022-11-21 20:45:36 +0700

    write const local_key, use localStorage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList';
 import {v4 as uuidv4} from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
 function App() {
-  const [todos, setTodos] = useState([{id:uuidv4(), name:'To do 1',complete: false}])
+  const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
+
+  //load todo
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+      if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  //save todo
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
   function handleAddTodo(e){
     const name = todoNameRef.current.value
     if (name === '') return
