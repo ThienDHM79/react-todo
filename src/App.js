9c2226e167390f7c0f123ccac894b0b73d3e701d
Thien DAO <thien1997nus@gmail.com>
2022-11-21 13:41:35 +0700

    create jsx <TodoList/>, file ToDoList.js with component, import to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import TodoList from './TodoList';
 function App() {
-  return null
+  return (
+    <TodoList />
+
+  )
 }
 
 export default App;
